b7dbc9fe1240091faef807946de0cf22f82d3812
samhall <sam.hall700@googlemail.com>
2022-01-26 10:30:29 +0000

    Added Depth data to icepoints.json
    Altered isHomogenous function to split if a cell contains land
    
    Fixed issue will cellSplitting
    
    added ability to highlight cells

--- refactor/CellGrid.py
+++ refactor/CellGrid.py
@@ -52,6 +52,22 @@ class CellGrid:
         selectedCell = []
         
         for cellBox in self.cellBoxes:
+            if cellBox.containsPoint(lat, long):
+                selectedCell.append(cellBox)
+        
+        # for inital debugging and should be replaced to throw errors correctly
+        if len(selectedCell) == 1:
+            return selectedCell[0]
+        elif len(selectedCell) == 0:
+            return "No cellBox was found at lat =" + str(lat) + ", long =" + str(long)
+        elif len(selectedCell) > 1:
+            return "ERROR: Multiple cellBoxes have been found at lat =" + str(lat) + ", long =" + str(long) 
+    
+    """
+    def getCellBox(self, lat, long):
+        selectedCell = []
+        
+        for cellBox in self.cellBoxes:
             if(cellBox.lat == lat and cellBox.long == long):
                 selectedCell.append(cellBox) 
         
@@ -62,7 +78,8 @@ class CellGrid:
             return "No cellBox was found at lat =" + str(lat) + ", long =" + str(long)
         elif len(selectedCell) > 1:
             return "ERROR: Multiple cellBoxes have been found at lat =" + str(lat) + ", long =" + str(long) 
-          
+    """
+    
     def recursiveSplit(self, maxSplits):
         splitCellBoxes = []
         for cellBox in self.cellBoxes:
@@ -85,6 +102,18 @@ class CellGrid:
         ax.set_facecolor('xkcd:blue')
         for cellBox in self.cellBoxes:
             ax.add_patch(cellBox.getPolygon())
+            ax.add_patch(cellBox.getBorder())
+        
+        ax.set_xlim(self._longMin, self._longMax)
+        ax.set_ylim(self._latMin, self._latMax)
+        
+    def highlightCell(self, selectedCellBox):
+        fig, ax = plt.subplots(1,1, figsize=(15,10))
+        ax.set_facecolor('xkcd:blue')
+        for cellBox in self.cellBoxes:
+            ax.add_patch(cellBox.getPolygon())
+        
+        ax.add_patch(selectedCellBox.getBorder())
         
         ax.set_xlim(self._longMin, self._longMax)
         ax.set_ylim(self._latMin, self._latMax)
