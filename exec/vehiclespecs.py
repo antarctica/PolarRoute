18c15973a754cdcc3b0fec5a454f791b6b01624d
George <gecoomb@bas.ac.uk>
2022-07-21 13:12:46 +0100

    Update naming within exec scripts

--- exec/vehiclespecs.py
+++ exec/vehiclespecs.py
@@ -1,9 +1,9 @@
 import sys,json
-from RoutePlanner.speed import SpeedFunctions
+from RoutePlanner.vessel_performance import VesselPerformance
 
 # Loading Configuration
 with open(sys.argv[1], 'r') as f:
     config = json.load(f)
 
 # Vehicle Specs
-sf = SpeedFunctions(config)
+sf = VesselPerformance(config)
