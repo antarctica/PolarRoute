884ee67f1defba41f5dd4fb1785edca0d75f4bc7
Samuel Hall <96304883+SamuelHall700@users.noreply.github.com>
2022-08-09 16:03:54 +0100

    Updating regression testing

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -2,8 +2,13 @@
     Regression testing package to ensure consistance functionality in development
     of the PolarRoute python package.
 """
-
+import sys
+import traceback
 import numpy as np
+import json
+
+from polar_route.mesh import Mesh
+from polar_route.vessel_performance import VesselPerformance
 
 def test_cellbox_count(mesh_a, mesh_b):
     """
@@ -83,18 +88,20 @@ def test_cellbox_values(mesh_a, mesh_b):
         if cellbox_a['id'] in indxed_b.keys():
             cellbox_b = indxed_b[cellbox_a['id']]
 
-            if not cellbox_a == cellbox_b:
-                mismatch_values = []
-                for key in cellbox_a.keys():
+            mismatch_values = []
+            for key in cellbox_a.keys():
+                # To prevent crashing if cellboxes have different attributes
+                # This error will be detected by the 'test_cellbox_attributes' test
+                if key in cellbox_b.keys():
                     value_a = cellbox_a[key]
                     value_b = cellbox_b[key]
 
-                    if not(value_a == value_b) and not(np.isnan(value_a) and np.isnan(value_b)):
+                    if not(value_a == value_b) and not(np.isnan(value_a) or np.isnan(value_b)):
                         mismatch_values.append(key)
-                mismatch_cellboxes[cellbox_a['id']] = mismatch_values
+                        mismatch_cellboxes[cellbox_a['id']] = mismatch_values
 
     assert(len(mismatch_cellboxes) == 0) , \
-        f"Values in some cellboxes in the new mesh have changed. The changes cellboxes are: {mismatch_cellboxes}"
+        f"Values in <{len(mismatch_cellboxes.keys())}> cellboxes in the new mesh have changed. The changes cellboxes are: {mismatch_cellboxes}"
 
 def test_cellbox_attributes(mesh_a, mesh_b):
     """
@@ -137,7 +144,7 @@ def test_neighbour_graph_count(graph_a, graph_b):
     graph_b_count = len(graph_b.keys())
 
     assert(graph_a_count == graph_b_count), \
-        f"Incorrect number of nodes in neighbour graph. Expected: {graph_a_count}, got {graph_b_count}"
+        f"Incorrect number of nodes in neighbour graph. Expected: <{graph_a_count}> nodes, got: <{graph_b_count}> nodes."
 
 def test_neighbour_graph_ids(graph_a, graph_b):
     """
@@ -155,7 +162,7 @@ def test_neighbour_graph_ids(graph_a, graph_b):
     missing_b_keys = list(graph_a_ids - graph_b_ids)
 
     assert(graph_a_ids == graph_b_ids) , \
-        f"Mismatch in neighbour graph nodes. Nodes {missing_a_keys} have appeared in the new neighbour graph. Nodes {missing_b_keys} are missing from the new neighbour graph"
+        f"Mismatch in neighbour graph nodes. <{len(missing_a_keys)}> nodes  have appeared in the new neighbour graph. <{len(missing_b_keys)}> nodes  are missing from the new neighbour graph."
 
 def test_neighbour_graph_values(graph_a, graph_b):
     """
@@ -179,4 +186,86 @@ def test_neighbour_graph_values(graph_a, graph_b):
                 mismatch_neighbors[node] = neighbours_b
 
     assert(len(mismatch_neighbors) == 0), \
-        f"Mismatch in neighbour graph neighbours. Nodes {mismatch_neighbors.keys()} have changed in new mesh."
+        f"Mismatch in neighbour graph neighbours. <{len(mismatch_neighbors.keys())}> nodes have changed in new mesh."
+
+def main():
+    """
+        main method for executions
+    """
+    args = sys.argv[1:]
+    file = args[0]
+
+    with open(file, 'r') as f:
+        info = json.load(f)
+
+    print("loading regression...")
+    config = info['config']
+
+    print("building Mesh...")
+    test_mesh = Mesh(config)
+
+    print("converting Mesh to json...")
+    t_mesh_json = test_mesh.to_json()
+
+    print("applying vessel performance...")
+    vp = VesselPerformance(t_mesh_json)
+    t_mesh_json = vp.to_json()
+
+    print("regression testing Mesh...")
+    # Testing cellboxes
+    regression_mesh = info['cellboxes']
+    new_mesh = t_mesh_json['cellboxes']
+    try:
+        test_cellbox_count(regression_mesh, new_mesh)
+        print("test_cellbox_count : passed")
+    except AssertionError as warning:
+        print("test_cellbox_count : failed")
+        print("    " + str(warning))
+
+    try:
+        test_cellbox_ids(regression_mesh, new_mesh)
+        print("test_cellbox_ids : passed")
+    except AssertionError as warning:
+        print("test_cellbox_ids : failed")
+        print("    " + str(warning))
+
+    try:
+        test_cellbox_attributes(regression_mesh, new_mesh)
+        print("test_cellbox_attributes : passed")
+    except AssertionError as warning:
+        print("test_cellbox_attributes : failed")
+        print("    " + str(warning))
+
+    try:
+        test_cellbox_values(regression_mesh, new_mesh)
+        print("test_cellbox_values : passed")
+    except AssertionError as warning:
+        print("test_cellbox_values' : failed")
+        print("    " + str(warning))
+
+    # Testing neighbour_graphs
+    regression_graph = info['neighbour_graph']
+    new_graph = t_mesh_json['neighbour_graph']
+    try:
+        test_neighbour_graph_count(regression_graph, new_graph)
+        print("test_neighbour_graph_count : passed")
+    except AssertionError as warning:
+        print("test_neighbour_graph_count : failed")
+        print("    " + str(warning))
+    try:
+        test_neighbour_graph_ids(regression_graph, new_graph)
+        print("test_neighbour_graph_ids : passed")
+    except AssertionError as warning:
+        print("test_neighbour_graph_ids : failed")
+        print("    " + str(warning))
+    try:
+        test_neighbour_graph_values(regression_graph, new_graph)
+        print("test_neighbour_graph_values : passed")
+    except AssertionError as warning:
+        print("test_neighbour_graph_values : failed")
+        print("    " + str(warning))
+    print("tests complete!")
+    
+
+if __name__ == "__main__":
+    main()
