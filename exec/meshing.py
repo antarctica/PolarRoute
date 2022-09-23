ceac034ec4a35e8a71bc649f5dad3ff209f91512
James Byrne <jambyr@bas.ac.uk>
2022-09-23 10:56:43 +0100

    Removing exec

--- exec/meshing.py
+++
@@ -1,9 +0,0 @@
-import sys,json
-from RoutePlanner.CellGrid import CellGrid
-
-# Loading Configuration
-with open(sys.argv[1], 'r') as f:
-    config = json.load(f)
-
-# Discrete Meshing
-mesh = CellGrid(config)
