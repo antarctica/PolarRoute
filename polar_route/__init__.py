e7f66340d8c145d4fb4c2c320c51b0392e6f52ca
Jonathan Smith <jon_smith83@hotmail.co.uk>
2022-10-18 15:48:35 +0100

    Updating Version number consistent with PyPi

--- polar_route/__init__.py
+++ polar_route/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.5a1"
+__version__ = "0.0.10"
 __description__ = "RoutePlanner: Optimal Route Planner for SDA"
 __license__ = "MIT"
 __author__ = "Maria Fox, James Byrne, Jonathan Smith, Samuel Hall, George Coombs"
