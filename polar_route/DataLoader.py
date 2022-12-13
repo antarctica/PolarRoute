c85e5a797ef871fb7436c8c80ce96bb687df3501
hjabbot <42661345+hjabbot@users.noreply.github.com>
2022-12-13 09:32:50 +0000

    Fixed DataLoaderFactory to accept params instead of **kwargs

--- polar_route/DataLoader.py
+++ polar_route/DataLoader.py
@@ -12,13 +12,28 @@ from polar_route.Boundary import Boundary
 
 class DataLoaderFactory:
 
-    def get_dataloader(name, file_location, **kwargs):
-        # TODO Replace **kwargs with params
-        if   name == 'GEBCO': return GEBCO_DataLoader(file_location, **kwargs)
-        elif name == 'AMSR':  return AMSR_DataLoader(file_location, **kwargs)
-        elif name == 'SOSE':  return SOSE_DataLoader(file_location, **kwargs)
-        # elif ...
-        else: raise ValueError(f'{name} not in known list of DataLoaders')
+	def get_dataloader(name, params, min_dp=5):
+		# TODO Replace **kwargs with params
+		if 'file' in params:  file_location = params['file']  
+		else:                 raise ValueError('File not specified')
+		
+		if 'downsample_factors' in params:  ds = params['downsample_factors']
+		else:                               ds = None
+
+		if 'data_name' in params: data_name = params['data_name'] 
+		else: 					  data_name = None
+		
+		if 'aggregate_type' in params: agg_type = params['aggregate_type']  
+		else:                          agg_type = 'MEAN'
+
+
+		if   name == 'GEBCO':  data_loader = GEBCO_DataLoader
+		elif name == 'AMSR':  data_loader = AMSR_DataLoader
+		elif name == 'SOSE':  data_loader = SOSE_DataLoader
+		# elif ...
+		else: raise ValueError(f'{name} not in known list of DataLoaders')
+
+		return data_loader(file_location, min_dp=min_dp, ds=ds, data_name=data_name, aggregate_type=agg_type)
 
 
 class ScalarDataLoader(ABC):
@@ -92,9 +107,11 @@ class ScalarDataLoader(ABC):
 
 		# Remove lat, long and time column if they exist
 		dps = self.get_datapoints(bounds)
-		
+		# If no data
+		if len(dps) == 0:
+			return None
 		# Return float of aggregated value
-		if self.aggregate_type == 'MIN':
+		elif self.aggregate_type == 'MIN':
 			return float(dps.min(skipna=skipna))
 		elif self.aggregate_type == 'MAX':
 			return float(dps.max(skipna=skipna))
@@ -186,7 +203,7 @@ class GEBCO_DataLoader(ScalarDataLoader):
 		logging.debug(f"- Opening file {self.file_location}")
 		raw_data = xr.open_dataset(self.file_location)
 		# Downsample data by extracting every (y,x)th element
-        # Downsampling before xarray.to_dataframe() cuts loading time hugely
+		# Downsampling before xarray.to_dataframe() cuts loading time hugely
 		if self.ds: # If downsampling not 'None'
 			logging.debug(f"- Downsampling GEBCO data by {self.ds} for (y,x), i.e. (lat, long)")
 			# Taking max because bathymetry
@@ -199,22 +216,25 @@ class GEBCO_DataLoader(ScalarDataLoader):
 
 
 if __name__=='__main__':
-    factory = DataLoaderFactory
-    gebco = factory.get_dataloader('GEBCO', 
-                                'PolarRoute/datastore/bathymetry/GEBCO/gebco_2022_n-40.0_s-90.0_w-140.0_e0.0.nc', 
-                                    min_dp = 5,
-                                    ds=(5,5), 
-                                    data_name="elevation", 
-                                    aggregate_type="MAX"
-                                )
-    bounds = Boundary([-85,-84.9], [-135,-134.9], ['1970-01-01','2021-12-31'])
+
+	params = {
+		'file': 'PolarRoute/datastore/bathymetry/GEBCO/gebco_2022_n-40.0_s-90.0_w-140.0_e0.0.nc',
+		'downsample_factors': (5,5),
+		'data_name': 'elevation',
+		'aggregate_type': 'MAX'
+	}
+
+	factory = DataLoaderFactory
+	gebco = factory.get_dataloader('GEBCO', params, min_dp = 5)
+
+	bounds = Boundary([-85,-84.9], [-135,-134.9], ['1970-01-01','2021-12-31'])
 
 
-    print(gebco.get_value(bounds))
+	print(gebco.get_value(bounds))
 
-    split_conds = {
-    'threshold': 620,
-    'upper_bound': 0.9,
-    'lower_bound': 0.1
-    }
-    print(gebco.get_hom_condition(bounds, split_conds))
+	split_conds = {
+	'threshold': 620,
+	'upper_bound': 0.9,
+	'lower_bound': 0.1
+	}
+	print(gebco.get_hom_condition(bounds, split_conds))
