c4bc810aa2ec4d3e6175253ec2665cb7053fac01
Ayat Fekry <ayat@Ayats-Air.nerc-bas.ac.uk>
2022-12-19 11:48:37 +0000

    edits in unit tests for Boundary

--- tests/unit_tests/test_boundary.py
+++ tests/unit_tests/test_boundary.py
@@ -1,9 +1,9 @@
 
 import unittest
 
-from PolarRoute.polar_route.Boundary import Boundary
+from polar_route.Boundary import Boundary
 class TestBoundary (unittest.TestCase):
-   def __init__(self):
+   def setUp(self):
          self.boundary = Boundary([-85,-84.9], [-135,-134.9], ['1970-01-01','2021-12-31'])
 
    def test_getcx (self):
@@ -18,21 +18,24 @@ class TestBoundary (unittest.TestCase):
       height = -84.9 +85
       self.assertEqual ( height , self.boundary.get_height())
 
-def test_get_width (self):
+   def test_get_width (self):
       
       width = -134.9 +135
       self.assertEqual ( width , self.boundary.get_width())
 
-def test_valid_bounds (self):
-    self.assertRaises(ValueError, Boundary ([3,2] , [-135,-134.9], ['1970-01-01','2021-12-31']))
-    self.assertRaises(ValueError , Boundary ([-85,-84.9] , [3,2], ['1970-01-01','2021-12-31']))
-    self.assertRaises(ValueError ,  Boundary ([-85,-84.9] ,[-135,-134.9], ['2021-12-31', '1970-01-01']))
+   def test_valid_bounds (self):
+    self.assertRaises(ValueError, Boundary , [3,2] , [-135,-134.9], ['1970-01-01','2021-12-31'])
+    self.assertRaises(ValueError , Boundary ,[-85,-84.9] , [3,2], ['1970-01-01','2021-12-31'])
+    self.assertRaises(ValueError ,  Boundary ,[-85,-84.9] ,[-135,-134.9], ['2021-12-31', '1970-01-01'])
 
 
-def test_get_bounds (self):
+   def test_get_bounds (self):
     bounds = [[-85,-135],
     [-84.9 , -135],
     [-84.9 , -134.9],
     [-85, -134.9],
     [-85,-135]]
-    self.assertEquals ( bounds, self.Boundary.test_get_bounds())
+    self.assertEqual ( bounds, self.boundary.get_bounds())
+
+if __name__ == '__main__':
+    unittest.main()
