3351ac439c6f85ffbd1ce7b9ecff00009e780936
hjabbot <42661345+hjabbot@users.noreply.github.com>
2023-02-16 11:45:30 +0000

    Copy paste from standard code to density/thickness

--- polar_route/Dataloaders/Scalar/Density.py
+++ polar_route/Dataloaders/Scalar/Density.py
@@ -3,7 +3,7 @@ from polar_route.utils import date_range
 
 
 from datetime import datetime, timedelta
-
+import logging
 
 
 import numpy as np
@@ -24,41 +24,41 @@ class DensityDataLoader(ScalarDataLoader):
         '''
         Placeholder until lookup-table dataloader class implemented
         '''
-        densities = {'summer': 875.0, 
-                     'autumn': 900.0, 
-                     'winter': 920.0,
-                     'spring': 900.0}
+        seasons = {1: 'su', 2: 'su', 3: 'a', 4: 'a', 5: 'a', 6: 'w', 7: 'w', 8: 'w', 9: 'sp', 10: 'sp', 11: 'sp',
+                12: 'su'}
+        densities = {'su': 875.0, 'sp': 900.0, 'a': 900.0, 'w': 920.0}
 
-        seasons = {
-            12: 'summer', 1:  'summer', 2:  'summer', 
-            3:  'autumn', 4:  'autumn', 5:  'autumn', 
-            6:  'winter', 7:  'winter', 8:  'winter', 
-            9:  'spring', 10: 'spring', 11: 'spring'}
-        
-        def get_density(d):
+        def ice_density(d):
             month = d.month
             season = seasons[month]
-            return densities[season]
-        
-        
-        lats = [lat for lat in np.arange(bounds.get_lat_min(), 
-                                         bounds.get_lat_max(), 0.05)]
-        lons = [lon for lon in np.arange(bounds.get_long_min(), 
-                                         bounds.get_long_max(), 0.05)]
+            den = densities[season]
+            return den
 
-        start_date = datetime.strptime(bounds.get_time_min(), "%Y-%m-%d")
-        end_date = datetime.strptime(bounds.get_time_max(), "%Y-%m-%d")
-        # delta = end_date - start_date
-        #TODO Add 1 to range(delta.days), standard code missed this
+        start_date = datetime.strptime(bounds.get_time_min(), "%Y-%m-%d").date()
+        end_date = datetime.strptime(bounds.get_time_max(), "%Y-%m-%d").date()
+
+        lats = [lat for lat in np.arange(bounds.get_lat_min(), bounds.get_lat_max(), 0.05)]
+        lons = [lon for lon in np.arange(bounds.get_long_min(), bounds.get_long_max(), 0.05)]
         dates = [single_date for single_date in date_range(start_date, end_date)]
-        
+
         density_data = xr.DataArray(
-            data=[[[get_density(d) for _ in lons] for _ in lats] for d in dates],
+            data=[[[ice_density(dt)
+                    for _ in lons]
+                for _ in lats]
+                for dt in dates],
             coords=dict(
                 lat=lats,
                 long=lons,
-                time=[d.strftime('%Y-%m-%d') for d in dates]),
-            dims=('time','lat','long'),
-            name='density')
-        
-        return density_data.to_dataframe().reset_index().set_index(['lat', 'long', 'time']).reset_index()
+                time=[dt.strftime("%Y-%m-%d") for dt in dates],
+            ),
+            dims=("time", "lat", "long"),
+            name="density",
+        )
+
+        density_df = density_data.\
+            to_dataframe().\
+            reset_index().\
+            set_index(['lat', 'long', 'time']).reset_index()
+
+        logging.debug("returned {} datapoints".format(len(density_df.index)))
+        return density_df
--- polar_route/Dataloaders/Scalar/Thickness.py
+++ polar_route/Dataloaders/Scalar/Thickness.py
@@ -2,7 +2,7 @@ from .AbstractScalar import ScalarDataLoader
 from polar_route.utils import date_range
 
 from datetime import datetime, timedelta
-
+import logging
 import numpy as np
 import xarray as xr
 
@@ -21,62 +21,67 @@ class ThicknessDataLoader(ScalarDataLoader):
         '''
         Placeholder until lookup-table dataloader class implemented
         '''
-        thicknesses = {
-            'Ross':          {'winter': 0.72, 'spring': 0.67, 'summer': 1.32, 
-                              'autumn': 0.82, 'year': 1.07},
-            'Bellinghausen': {'winter': 0.65, 'spring': 0.79, 'summer': 2.14, 
-                              'autumn': 0.79, 'year': 0.90},
-            'Weddell E':     {'winter': 0.54, 'spring': 0.89, 'summer': 0.87, 
-                              'autumn': 0.44, 'year': 0.73},
-            'Weddell W':     {'winter': 1.33, 'spring': 1.33, 'summer': 1.20, 
-                              'autumn': 1.38, 'year': 1.33},
-            'Indian':        {'winter': 0.59, 'spring': 0.78, 'summer': 1.05, 
-                              'autumn': 0.45, 'year': 0.68},
-            'West Pacific':  {'winter': 0.72, 'spring': 0.68, 'summer': 1.17, 
-                              'autumn': 0.75, 'year': 0.79},
-            'None':          {'winter': 0.72, 'spring': 0.67, 'summer': 1.32, 
-                              'autumn': 0.82, 'year': 1.07}}
-        seasons = {
-            12: 'summer', 1:  'summer', 2:  'summer', 
-            3:  'autumn', 4:  'autumn', 5:  'autumn', 
-            6:  'winter', 7:  'winter', 8:  'winter', 
-            9:  'spring', 10: 'spring', 11: 'spring'}
-        
-        def get_thickness(d, lon):
+        thicknesses = {'Ross': {'w': 0.72, 'sp': 0.67, 'su': 1.32, 'a': 0.82, 'y': 1.07},
+                    'Bellinghausen': {'w': 0.65, 'sp': 0.79, 'su': 2.14, 'a': 0.79, 'y': 0.90},
+                    'Weddell E': {'w': 0.54, 'sp': 0.89, 'su': 0.87, 'a': 0.44, 'y': 0.73},
+                    'Weddell W': {'w': 1.33, 'sp': 1.33, 'su': 1.20, 'a': 1.38, 'y': 1.33},
+                    'Indian': {'w': 0.59, 'sp': 0.78, 'su': 1.05, 'a': 0.45, 'y': 0.68},
+                    'West Pacific': {'w': 0.72, 'sp': 0.68, 'su': 1.17, 'a': 0.75, 'y': 0.79},
+                    'None': {'w': 0.72, 'sp': 0.67, 'su': 1.32, 'a': 0.82, 'y': 1.07}}
+        seasons = {1: 'su', 2: 'su', 3: 'a', 4: 'a', 5: 'a', 6: 'w', 7: 'w', 8: 'w', 9: 'sp', 10: 'sp', 11: 'sp',
+                12: 'su'}
+
+        def ice_thickness(d, long):
+            """
+                Returns ice thickness. Data taken from Table 3 in: doi:10.1029/2007JC004254
+            """
+            # The table has missing data points for Bellinghausen Autumn and Weddell W Winter, may require further thought
             month = d.month
             season = seasons[month]
-            
-            if   -130 <= lon <  -60: sea = 'Bellinghausen'
-            elif  -60 <= lon <  -45: sea = 'Weddell W'
-            elif  -45 <= lon <   20: sea = 'Weddell E'
-            elif   20 <= lon <   90: sea = 'Indian'
-            elif   90 <= lon <  160: sea = 'West Pacific'
-            elif  160 <= lon <  180: sea = 'Ross'
-            elif -180 <= lon < -130: sea = 'Ross'
-            else: sea = 'None'
-            
+            sea = None
+
+            if -130 <= long < -60:
+                sea = 'Bellinghausen'
+            elif -60 <= long < -45:
+                sea = 'Weddell W'
+            elif -45 <= long < 20:
+                sea = 'Weddell E'
+            elif 20 <= long < 90:
+                sea = 'Indian'
+            elif 90 <= long < 160:
+                sea = 'West Pacific'
+            elif (160 <= long < 180) or (-180 <= long < -130):
+                sea = 'Ross'
+            else:
+                sea = 'None'
+
             return thicknesses[sea][season]
-        
-        
-        lats = [lat for lat in np.arange(bounds.get_lat_min(), 
-                                         bounds.get_lat_max(), 0.05)]
-        lons = [lon for lon in np.arange(bounds.get_long_min(), 
-                                         bounds.get_long_max(), 0.05)]
-        
-        start_date = datetime.strptime(bounds.get_time_min(), "%Y-%m-%d")
-        end_date = datetime.strptime(bounds.get_time_max(), "%Y-%m-%d")
-        # delta = end_date - start_date
-        #TODO Add 1 to range(delta.days), standard code missed this
-        # dates = [start_date + timedelta(days=i) for i in range(delta.days)]
+
+        start_date = datetime.strptime(bounds.get_time_min(), "%Y-%m-%d").date()
+        end_date = datetime.strptime(bounds.get_time_max(), "%Y-%m-%d").date()
+
+        lats = [lat for lat in np.arange(bounds.get_lat_min(), bounds.get_lat_max(), 0.05)]
+        lons = [lon for lon in np.arange(bounds.get_long_min(), bounds.get_long_max(), 0.05)]
         dates = [single_date for single_date in date_range(start_date, end_date)]
-        
-        thickness_data = xr.DataArray(
-            data=[[[get_thickness(d, lon) for lon in lons] for _ in lats] for d in dates],
+
+        thick_data = xr.DataArray(
+            data=[[[ice_thickness(dt, lng)
+                    for lng in lons]
+                for _ in lats]
+                for dt in dates],
             coords=dict(
                 lat=lats,
                 long=lons,
-                time=[d.strftime('%Y-%m-%d') for d in dates]),
-            dims=('time','lat','long'),
-            name='thickness')
+                time=[dt.strftime("%Y-%m-%d") for dt in dates],
+            ),
+            dims=("time", "lat", "long"),
+            name="thickness",
+        )
 
-        return thickness_data.to_dataframe().reset_index().set_index(['lat', 'long', 'time']).reset_index()
+        thick_df = thick_data.\
+            to_dataframe().\
+            reset_index().\
+            set_index(['lat', 'long', 'time']).reset_index()
+
+        logging.debug("returning {} datapoints".format(len(thick_df.index)))
+        return thick_df
