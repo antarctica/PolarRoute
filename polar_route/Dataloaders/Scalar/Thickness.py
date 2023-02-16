8025f21928c5c42b612f765a9b96f96c480bb058
hjabbot <42661345+hjabbot@users.noreply.github.com>
2023-02-16 11:26:51 +0000

    Reverting changes, trying new changes (data_name allocation)

--- polar_route/Dataloaders/Scalar/Thickness.py
+++ polar_route/Dataloaders/Scalar/Thickness.py
@@ -14,7 +14,8 @@ class ThicknessDataLoader(ScalarDataLoader):
             setattr(self, key, val)
         
         self.data = self.import_data(bounds)
-        self.data_name = self.get_data_col_name()
+        # self.data_name = self.get_data_col_name()
+        self.data_name = 'thickness'
                     
     def import_data(self, bounds):
         '''
@@ -58,9 +59,9 @@ class ThicknessDataLoader(ScalarDataLoader):
         
         
         lats = [lat for lat in np.arange(bounds.get_lat_min(), 
-                                         bounds.get_lat_max(), 0.05)][1:]
+                                         bounds.get_lat_max(), 0.05)]
         lons = [lon for lon in np.arange(bounds.get_long_min(), 
-                                         bounds.get_long_max(), 0.05)][1:]
+                                         bounds.get_long_max(), 0.05)]
         
         start_date = datetime.strptime(bounds.get_time_min(), "%Y-%m-%d")
         end_date = datetime.strptime(bounds.get_time_max(), "%Y-%m-%d")
