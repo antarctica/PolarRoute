171c7bb768a6c27f43686dd73ca8e1c1badddc40
George <gecoomb@bas.ac.uk>
2023-02-20 17:04:53 +0000

    Define methods for AbstractVessel

--- polar_route/vessel_performance/AbstractVessel.py
+++ polar_route/vessel_performance/AbstractVessel.py
@@ -1,4 +1,35 @@
-from abc import ABCMeta
+from polar_route.AggregatedCellBox import AggregatedCellBox
+from abc import ABCMeta, abstractmethod
 
 class AbstractVessel(metaclass=ABCMeta):
-    pass
+
+    @abstractmethod
+    def __init__(self, params: dict):
+        """
+        Initialise the vessel object with parameters from the config.
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def model_performance(self, cellbox: AggregatedCellBox):
+        """
+        Calculate performance parameters for the given vessel.
+        Args:
+            cellbox (AggregatedCellBox): cell in which performance is being modelled
+
+        Returns:
+            performance_values (dict): values for relevant performance parameters
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def model_accessibility(self, cellbox: AggregatedCellBox):
+        """
+        Determine accessibility of the input cell for the given vessel.
+        Args:
+            cellbox (AggregatedCellBox): cell in which accessibility is being determined
+
+        Returns:
+            access_values (dict): values for the accessibility and other related booleans
+        """
+        raise NotImplementedError
