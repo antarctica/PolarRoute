db37fd1bee059b67e189c193254852d58a8e28b7
hjabbot <42661345+hjabbot@users.noreply.github.com>
2023-02-23 13:18:49 +0000

    Working on getting LUT thickness dataloader

--- polar_route/Dataloaders/LookupTable/AbstractLUT.py
+++ polar_route/Dataloaders/LookupTable/AbstractLUT.py
@@ -17,18 +17,79 @@ class LookupTableDataLoader(DataLoaderInterface):
     
     @abstractmethod
     def setup_lookup_table(self):
-        # Returns dict of
-        # {Boundary: value}
+        '''
+        Returns:
+            (dict)
+        {
+            'area_name':{
+                'boundary': Boundary(
+                    [lat_min, lat_max],
+                    [long_min, long_max],
+                    ['2000-01-01', '2000-12-31'] # Dates unimportant, only coords used
+                    ),
+                'value': {
+                    'summer': int,
+                    'autumn': int,
+                    'winter': int,
+                    'spring': int,
+                }
+            }
+        }
+        '''
+
         pass
 
     def get_value(self, bounds):
-        pass        
-
-    def coord_to_boundary(self, coords):
-        # If coord lat < boundary_lat_min, drop
-        # If coords lat > boundary_lat_max, drop
-        # Same for long
-        # Same for time
-        # Return list of boundaries that haven't been dropped
-        pass
+        pass      
     
+    def get_hom_condition(self):
+        pass  
+
+    def coords_in_boundary(self, coords, bounds):
+        
+        if bounds.get_lat_min() <= coords['lat'] < bounds.get_lat_max() and \
+           bounds.get_long_min()<= coords['long']< bounds.get_long_max():
+            return True
+        else:
+            return False
+        
+    def bounds_in_boundary(self, local_bounds, global_boundary):
+        
+        bot_l = {'lat': local_bounds.get_lat_min(), 'long': local_bounds.get_long_min()}
+        top_l = {'lat': local_bounds.get_lat_min(), 'long': local_bounds.get_long_max()}
+        bot_r = {'lat': local_bounds.get_lat_max(), 'long': local_bounds.get_long_min()}
+        top_r = {'lat': local_bounds.get_lat_max(), 'long': local_bounds.get_long_max()}
+        
+        if self.coords_in_boundary(bot_l, global_boundary) or \
+            self.coords_in_boundary(top_l, global_boundary) or \
+            self.coords_in_boundary(bot_r, global_boundary) or \
+            self.coords_in_boundary(top_r, global_boundary):
+            return True
+        else:
+            return False
+
+    
+    def downsample(self):
+        raise NotImplementedError(
+            "downsample() method doesn't make sense for LUT Dataloader!"
+            )
+    def get_datapoints(self):
+        raise NotImplementedError(
+            "get_datapoints() method doesn't make sense for LUT Dataloader!"
+            )
+    def import_data(self):
+        raise NotImplementedError(
+            "import_data() method doesn't make sense for LUT Dataloader!"
+            )
+    def reproject(self):
+        raise NotImplementedError(
+            "reproject() method doesn't make sense for LUT Dataloader!"
+            )
+    def get_data_col_name(self):
+        raise NotImplementedError(
+            "get_data_col_name() method doesn't make sense for LUT Dataloader!"
+            )
+    def set_data_col_name(self):
+        raise NotImplementedError(
+            "set_data_col_name() method doesn't make sense for LUT Dataloader!"
+            )
