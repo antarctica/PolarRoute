b2d7a18bb2aca102620fa108960d992428532952
Samuel Hall <96304883+SamuelHall700@users.noreply.github.com>
2023-04-05 14:50:23 +0100

    aliasing objects in mesh/__init__.py to shorten import length

--- polar_route/__init__.py
+++ polar_route/__init__.py
@@ -5,3 +5,6 @@ __author__ = "Jonathan Smith, Samuel Hall, George Coombs, Harrison Abbot, Ayat F
 __email__ = "jonsmi@bas.ac.uk"
 __copyright__ = "2022, BAS AI Lab"
 
+from polar_route.mesh_generation.mesh_builder import MeshBuilder as MeshBuilder
+from polar_route.dataloaders.factory import DataLoaderFactory as DataLoaderFactory
+from polar_route.vessel_performance.vessel_factory import VesselFactory as VesselFactory
