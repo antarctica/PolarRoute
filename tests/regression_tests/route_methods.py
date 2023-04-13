af1d342576956b8cbcc3ef63477f8bf9289d4d67
hjabbot <42661345+hjabbot@users.noreply.github.com>
2023-04-13 15:51:31 +0100

    Added dijkstra and smoothed route regression tests

---
+++ tests/regression_tests/route_methods.py
@@ -0,0 +1,251 @@
+import numpy as np
+import pandas as pd
+
+from polar_route.utils import round_to_sigfig
+
+SIG_FIG_TOLERANCE = 5
+
+# Testing fuel optimised routes
+def test_fuel_route_coordinates(fuel_route_pair):
+    compare_route_coordinates(fuel_route_pair[0], fuel_route_pair[1])
+
+def test_fuel_waypoint_names(fuel_route_pair):
+    compare_waypoint_names(fuel_route_pair[0], fuel_route_pair[1])
+    
+def test_fuel_time(fuel_route_pair):
+    compare_time(fuel_route_pair[0], fuel_route_pair[1])
+
+def test_fuel_fuel(fuel_route_pair):
+    compare_fuel(fuel_route_pair[0], fuel_route_pair[1])
+
+def test_fuel_cell_indices(fuel_route_pair):
+    compare_cell_indices(fuel_route_pair[0], fuel_route_pair[1])
+
+def test_fuel_cases(fuel_route_pair):
+    compare_cases(fuel_route_pair[0], fuel_route_pair[1])
+    
+def test_fuel_distance(fuel_route_pair):
+    compare_distance(fuel_route_pair[0], fuel_route_pair[1])
+    
+def test_fuel_speed(fuel_route_pair):
+    compare_speed(fuel_route_pair[0], fuel_route_pair[1])
+    
+# Testing time optimised routes
+def test_time_route_coordinates(time_route_pair):
+    compare_route_coordinates(time_route_pair[0], time_route_pair[1])
+
+def test_time_waypoint_names(time_route_pair):
+    compare_waypoint_names(time_route_pair[0], time_route_pair[1])
+    
+def test_time_time(time_route_pair):
+    compare_time(time_route_pair[0], time_route_pair[1])
+
+def test_time_fuel(time_route_pair):
+    compare_fuel(time_route_pair[0], time_route_pair[1])
+    
+def test_time_cell_indices(time_route_pair):
+    compare_cell_indices(time_route_pair[0], time_route_pair[1])
+
+def test_time_cases(time_route_pair):
+    compare_cases(time_route_pair[0], time_route_pair[1])
+    
+def test_time_distance(time_route_pair):
+    compare_distance(time_route_pair[0], time_route_pair[1])
+    
+def test_time_speed(time_route_pair):
+    compare_speed(time_route_pair[0], time_route_pair[1])
+    
+
+# Comparison between old and new
+def compare_route_coordinates(route_a, route_b):
+    """
+    Tests if coordinates of each node are the same between both routes.
+    
+    Args:
+        route_a (json)
+        route_b (json)
+        
+    Raises:
+        AssertionError:
+            Fails if length of coord arrays is different,
+            which implies different nodes along route
+        AssertionError:
+            Fails if coords of each node are different at any point 
+            (beyond sig fig limit)
+    """
+    coords_a = extract_path(route_a)['geometry']['coordinates']
+    coords_b = extract_path(route_b)['geometry']['coordinates']
+
+    len_a = len(coords_a)
+    len_b = len(coords_b)
+    
+    assert(len_a == len_b), \
+        f"Number of nodes different! Expected {len_a}, got {len_b}"
+    
+    rounded_x_a = round_to_sigfig(coords_a[:][0], sigfig=SIG_FIG_TOLERANCE)
+    rounded_x_b = round_to_sigfig(coords_b[:][0], sigfig=SIG_FIG_TOLERANCE)
+
+    rounded_y_a = round_to_sigfig(coords_a[:][1], sigfig=SIG_FIG_TOLERANCE)
+    rounded_y_b = round_to_sigfig(coords_b[:][1], sigfig=SIG_FIG_TOLERANCE)
+    
+    np.testing.assert_array_equal(rounded_x_a, rounded_x_b)
+    np.testing.assert_array_equal(rounded_y_a, rounded_y_b)
+
+def compare_waypoint_names(route_a, route_b):
+    """
+    Tests if waypoint names are the same between both routes.
+    
+    Args:
+        route_a (json)
+        route_b (json)
+        
+    Raises:
+        AssertionError:
+            Fails if source or destination names don't match 
+    """
+    from_a = extract_path(route_a)['properties']['from']
+    from_b = extract_path(route_b)['properties']['from']
+    assert(from_a == from_b), \
+        f"Waypoint source names don't match! Expected {from_a}, got {from_b}"
+    
+    to_a = extract_path(route_a)['properties']['to']
+    to_b = extract_path(route_b)['properties']['to']
+    assert(to_a == to_b), \
+        f"Waypoint destination names don't match! Expected {to_a}, got {to_b}"
+
+def compare_time(route_a, route_b):
+    """
+    Tests if times to each node are the same between both routes.
+    
+    Args:
+        route_a (json)
+        route_b (json)
+        
+    Raises:
+        AssertionError:
+            Fails if time to each node is different at any point 
+            (beyond sig fig limit)
+    """
+    times_a = extract_path(route_a)['properties']['traveltime']
+    times_b = extract_path(route_b)['properties']['traveltime']
+
+    rounded_a = round_to_sigfig(times_a, sigfig=SIG_FIG_TOLERANCE)
+    rounded_b = round_to_sigfig(times_b, sigfig=SIG_FIG_TOLERANCE)
+
+    np.testing.assert_array_equal(rounded_a, rounded_b)
+
+def compare_fuel(route_a, route_b):
+    """
+    Tests if fuel to each node are the same between both routes.
+    
+    Args:
+        route_a (json)
+        route_b (json)
+        
+    Raises:
+        AssertionError:
+            Fails if fuel to each node is different at any point 
+            (beyond sig fig limit)
+    """
+    fuel_a = extract_path(route_a)['properties']['fuel']
+    fuel_b = extract_path(route_b)['properties']['fuel']
+
+    rounded_a = round_to_sigfig(fuel_a, sigfig=SIG_FIG_TOLERANCE)
+    rounded_b = round_to_sigfig(fuel_b, sigfig=SIG_FIG_TOLERANCE)
+
+    np.testing.assert_array_equal(rounded_a, rounded_b)
+
+def compare_cell_indices(route_a, route_b):
+    """
+    Tests if cell indices are the same between both routes.
+    
+    Args:
+        route_a (json)
+        route_b (json)
+        
+    Raises:
+        AssertionError:
+            Fails if cell indices are different at any point
+    """
+    cid_a = extract_path(route_a)['properties']['CellIndices']
+    cid_b = extract_path(route_b)['properties']['CellIndices']
+
+    np.testing.assert_array_equal(cid_a, cid_b)
+
+def compare_cases(route_a, route_b):
+    """
+    Tests if dijkstra route direction cases are the same between both routes.
+    
+    Args:
+        route_a (json)
+        route_b (json)
+        
+    Raises:
+        AssertionError:
+            Fails if dijkstra route direction cases are different at any point
+    """
+    cases_a = extract_path(route_a)['properties']['cases']
+    cases_b = extract_path(route_b)['properties']['cases']
+    
+    np.testing.assert_array_equal(cases_a, cases_b)
+
+def compare_distance(route_a, route_b):
+    """
+    Tests if distance between cells are the same between both routes.
+    
+    Args:
+        route_a (json)
+        route_b (json)
+        
+    Raises:
+        AssertionError:
+            Fails if distances are different at any point
+    """
+    distance_a = extract_path(route_a)['properties']['distance']
+    distance_b = extract_path(route_b)['properties']['distance']
+
+    np.testing.assert_array_equal(distance_a, distance_b)
+    
+def compare_speed(route_a, route_b):
+    """
+    Tests if speed between cells are the same between both routes.
+    
+    Args:
+        route_a (json)
+        route_b (json)
+        
+    Raises:
+        AssertionError:
+            Fails if speeds are different at any point
+    """
+    speed_a = extract_path(route_a)['properties']['speed']
+    speed_b = extract_path(route_b)['properties']['speed']
+
+    np.testing.assert_array_equal(speed_a, speed_b)
+
+# Utility functions
+def extract_waypoints(mesh):
+    """
+    Extracts waypoints from mesh and returns as a pd.Dataframe for 
+    routeplanner to work with
+    
+    Args:
+        mesh (json): JSON object with waypoints key
+
+    Returns:
+        pd.DataFrame: Dataframe constructed from waypoints
+    """
+    return pd.DataFrame.from_dict(mesh['waypoints'])
+
+def extract_path(mesh):
+    """
+    Extracts path information from mesh and returns as a json for 
+    tests to work with
+    
+    Args:
+        mesh (json): JSON object with waypoints key
+
+    Returns:
+        json: Dictionary with path information
+    """
+    return mesh['paths']['features'][0]
---
+++ tests/regression_tests/test_routes_dijkstra.py
@@ -0,0 +1,101 @@
+import json
+import pytest
+
+from polar_route.route_planner import RoutePlanner
+from .route_methods import extract_waypoints
+
+# Import tests, which are automatically run
+from .route_methods import test_fuel_route_coordinates
+from .route_methods import test_fuel_waypoint_names
+from .route_methods import test_fuel_time
+from .route_methods import test_fuel_fuel
+from .route_methods import test_fuel_cell_indices
+from .route_methods import test_fuel_cases
+from .route_methods import test_time_route_coordinates
+from .route_methods import test_time_waypoint_names
+from .route_methods import test_time_time
+from .route_methods import test_time_fuel
+from .route_methods import test_time_cell_indices
+from .route_methods import test_time_cases
+
+FUEL_ROUTE_INFO = './example_routes/route_info_fuel.json'
+TIME_ROUTE_INFO = './example_routes/route_info_time.json'
+
+TEST_FUEL_ROUTES = [
+    './example_routes/dijkstra/fuel/gaussian_random_field.json',
+    './example_routes/dijkstra/fuel/checkerboard.json',
+    './example_routes/dijkstra/fuel/great_circle_forward.json',
+    './example_routes/dijkstra/fuel/great_circle_reverse.json',
+    # './example_routes/dijkstra/fuel/real.json'
+]
+
+TEST_TIME_ROUTES = [
+    './example_routes/dijkstra/time/gaussian_random_field.json',
+    './example_routes/dijkstra/time/checkerboard.json',
+    './example_routes/dijkstra/time/great_circle_forward.json',
+    './example_routes/dijkstra/time/great_circle_reverse.json',
+    # './example_routes/dijkstra/time/real.json'
+]
+
+# Pairing old and new outputs
+@pytest.fixture(scope='session', autouse=False, params=TEST_FUEL_ROUTES)
+def fuel_route_pair(request):
+    """
+    Creates a pair of JSON objects, one newly generated, one as old reference
+    Args:
+        request (fixture): 
+            fixture object including list of jsons of fuel optimised routes
+
+    Returns:
+        list: [reference json, new json]
+    """
+    # Load reference JSON
+    with open(request.param, 'r') as fp:
+        old_route = json.load(fp)
+    # Create new json (cast old to dict to create copy to avoid modifying)
+    new_route = calculate_dijkstra_route(FUEL_ROUTE_INFO, dict(old_route))
+    return [old_route, new_route]
+
+@pytest.fixture(scope='session', autouse=False, params=TEST_TIME_ROUTES)
+def time_route_pair(request):
+    """
+    Creates a pair of JSON objects, one newly generated, one as old reference
+    Args:
+        request (fixture): 
+            fixture object including list of jsons of time optimised routes
+
+    Returns:
+        list: [reference json, new json]
+    """
+    # Load reference JSON
+    with open(request.param, 'r') as fp:
+        old_route = json.load(fp)
+    # Create new json (cast old to dict to create copy to avoid modifying)
+    new_route = calculate_dijkstra_route(TIME_ROUTE_INFO, dict(old_route))
+    return [old_route, new_route]
+
+# Generating new outputs
+def calculate_dijkstra_route(route_info, route_json):
+    """
+    Calculates the fuel-optimised route, with dijkstra but no smoothing
+
+    Args:
+        route_filename (str): Filename of regression test route
+
+    Returns:
+        json: New route output
+    """
+
+    # Initial set up
+    config      = route_info
+    mesh        = route_json
+    waypoints   = extract_waypoints(mesh)
+    
+    # Calculate dijskstra route
+    rp = RoutePlanner(mesh, config, waypoints)
+    rp.compute_routes()
+    
+    # Generate json to compare to old output
+    new_route = rp.to_json()
+
+    return new_route
