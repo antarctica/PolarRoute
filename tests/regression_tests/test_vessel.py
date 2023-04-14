e214d96706ce281ed1f48a344639c04c8b411fa7
hjabbot <42661345+hjabbot@users.noreply.github.com>
2023-04-14 14:02:12 +0100

    Removed need to recompute env mesh for vessel meshes

--- tests/regression_tests/test_vessel.py
+++ tests/regression_tests/test_vessel.py
@@ -7,8 +7,6 @@ import json
 import pytest
 
 # from polar_route.mesh import Mesh
-from polar_route.mesh_generation.mesh_builder import MeshBuilder
-from polar_route.mesh_generation.environment_mesh import EnvironmentMesh
 from polar_route.vessel_performance.vessel_performance_modeller import VesselPerformanceModeller
 
 # Import tests, which are automatically run
@@ -21,31 +19,40 @@ from .mesh_test_functions import test_mesh_neighbour_graph_ids
 from .mesh_test_functions import test_mesh_neighbour_graph_values
 
 #File locations of all vessel performance meshes to be recalculated for regression testing.
-TEST_VESSEL_MESHES = [
+INPUT_MESHES = [
+    './example_meshes/env_meshes/mesh_2013.json',
+    './example_meshes/env_meshes/mesh_2017.json',
+    './example_meshes/env_meshes/mesh_2019.json'
+]
+
+OUTPUT_MESHES = [
     './example_meshes/vessel_meshes/vessel_2013.json',
     './example_meshes/vessel_meshes/vessel_2017.json',
     './example_meshes/vessel_meshes/vessel_2019.json'
 ]
 
-@pytest.fixture(scope='session', autouse=False, params=TEST_VESSEL_MESHES)
+@pytest.fixture(scope='session', autouse=False, params=zip(INPUT_MESHES, OUTPUT_MESHES))
 def mesh_pair(request):
-    with open(request.param, 'r') as fp:
-        old_mesh = json.load(fp)
-    
-    new_mesh = calculate_mesh(old_mesh)
 
+    input_mesh_file = request.param[0]
+    output_mesh_file = request.param[1]
+
+    with open(output_mesh_file, 'r') as fp:
+        old_mesh = json.load(fp)
+
+    with open(input_mesh_file, 'r') as fp:
+        input_mesh = json.load(fp)
+    
+    vessel_config = old_mesh['config']['Vessel']
+    new_mesh = calculate_mesh(input_mesh, vessel_config)
+
+
+    
     return [old_mesh, new_mesh]
 
-def calculate_mesh(mesh_json):
-    config = mesh_json['config']
-    mesh_builder = MeshBuilder(config)
-    env_mesh = mesh_builder.build_environmental_mesh()
+def calculate_mesh(mesh_json, vessel_config):
 
-    env_mesh = env_mesh.to_json()
-
-    vessel_config = mesh_json['config']['Vessel']
-    # env_mesh = EnvironmentMesh.load_from_json(mesh_json).to_json()
-    new_mesh = VesselPerformanceModeller(env_mesh, vessel_config)
+    new_mesh = VesselPerformanceModeller(mesh_json, vessel_config)
     new_mesh.model_accessibility()
     new_mesh.model_performance()
 
