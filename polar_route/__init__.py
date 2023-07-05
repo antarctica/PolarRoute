b50e8b5fff2d5fa9aab98018d7a5ca854a625ee5
hjabbot <42661345+hjabbot@users.noreply.github.com>
2023-07-05 16:38:00 +0100

    Updating version number and year

--- polar_route/__init__.py
+++ polar_route/__init__.py
@@ -1,9 +1,9 @@
-__version__ = "0.2.12"
+__version__ = "0.3.1"
 __description__ = "PolarRoute: Long-distance maritime polar route planning taking into account complex changing environmental conditions"
 __license__ = "MIT"
 __author__ = "Jonathan Smith, Samuel Hall, George Coombs, Harrison Abbot, Ayat Fekry, James Byrne, Michael Thorne, Maria Fox"
 __email__ = "jonsmi@bas.ac.uk"
-__copyright__ = "2022, BAS AI Lab"
+__copyright__ = "2023, BAS AI Lab"
 
 from polar_route.mesh_generation.mesh_builder import MeshBuilder as MeshBuilder
 from polar_route.dataloaders.factory import DataLoaderFactory as DataLoaderFactory
