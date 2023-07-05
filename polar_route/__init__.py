ca6548d694dc873c714d6a14e270f3f22a499477
George <gecoomb@bas.ac.uk>
2023-07-05 14:12:42 +0100

    Change version number

--- polar_route/__init__.py
+++ polar_route/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.2.11"
+__version__ = "0.3.0"
 __description__ = "PolarRoute: Long-distance maritime polar route planning taking into account complex changing environmental conditions"
 __license__ = "MIT"
 __author__ = "Jonathan Smith, Samuel Hall, George Coombs, Harrison Abbot, Ayat Fekry, James Byrne, Michael Thorne, Maria Fox"
