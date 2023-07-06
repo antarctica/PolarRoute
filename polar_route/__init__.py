e4510304e4d3cbd44f96e0214184d0b963da2792
hjabbot <42661345+hjabbot@users.noreply.github.com>
2023-07-06 10:32:09 +0100

    Updating copyright year

--- polar_route/__init__.py
+++ polar_route/__init__.py
@@ -3,7 +3,7 @@ __description__ = "PolarRoute: Long-distance maritime polar route planning takin
 __license__ = "MIT"
 __author__ = "Jonathan Smith, Samuel Hall, George Coombs, Harrison Abbot, Ayat Fekry, James Byrne, Michael Thorne, Maria Fox"
 __email__ = "jonsmi@bas.ac.uk"
-__copyright__ = "2023, BAS AI Lab"
+__copyright__ = "2022-2023, BAS AI Lab"
 
 from polar_route.mesh_generation.mesh_builder import MeshBuilder as MeshBuilder
 from polar_route.dataloaders.factory import DataLoaderFactory as DataLoaderFactory
