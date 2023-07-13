73a07f9cb0f7c2f2da0b2e7e48b5ff46d253f93f
hjabbot <42661345+hjabbot@users.noreply.github.com>
2023-07-13 10:34:17 +0100

    Catching error to avoid crashing setup.py

--- polar_route/__init__.py
+++ polar_route/__init__.py
@@ -5,10 +5,15 @@ __author__ = "Jonathan Smith, Samuel Hall, George Coombs, Harrison Abbot, Ayat F
 __email__ = "jonsmi@bas.ac.uk"
 __copyright__ = "2022-2023, BAS AI Lab"
 
-from polar_route.mesh_generation.mesh_builder import MeshBuilder as MeshBuilder
-from polar_route.dataloaders.factory import DataLoaderFactory as DataLoaderFactory
+# Wrapped in try-except so that setup.py can import polar_route without crashing due to dependency errors
+try:
+    from polar_route.mesh_generation.mesh_builder import MeshBuilder as MeshBuilder
+    from polar_route.dataloaders.factory import DataLoaderFactory as DataLoaderFactory
 
-from polar_route.vessel_performance.vessel_performance_modeller import VesselPerformanceModeller as VesselPerformanceModeller
+    from polar_route.vessel_performance.vessel_performance_modeller import VesselPerformanceModeller as VesselPerformanceModeller
 
-from polar_route.route_planner import RoutePlanner as RoutePlanner
-from polar_route.mesh_generation.boundary import Boundary as Boundary
+    from polar_route.route_planner import RoutePlanner as RoutePlanner
+    from polar_route.mesh_generation.boundary import Boundary as Boundary
+
+except ModuleNotFoundError as err:
+    print(f'{err}\n Is PolarRoute installed correctly?')
