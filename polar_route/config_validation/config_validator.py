2b0291d3c392eec59456f0e7f6cd58b8069f8707
hjabbot <42661345+hjabbot@users.noreply.github.com>
2023-08-01 14:13:20 +0100

    Initial schema set up for mesh config

---
+++ polar_route/config_validation/config_validator.py
@@ -0,0 +1,18 @@
+import json
+import jsonschema
+from jsonschema import validate
+
+def validate_mesh_config():
+    pass
+
+def validate_vessel_config():
+    pass
+
+def validate_route_config():
+    pass
+
+def validate_waypoints():
+    pass
+
+if __name__ == '__main__':
+    print('lol')
---
+++ polar_route/config_validation/mesh_schema.py
@@ -0,0 +1,89 @@
+region_schema = {
+    "type": "object",
+    "required": ["latMin",  "latMax", 
+                 "longMin", "longMax",
+                 "startTime", "endTime",
+                 "cellWidth", "cellHeight"],
+    "additionalProperties": False,
+    "properties":{    
+        "latMin": {"type": "number"},
+        "latMax": {"type": "number"},
+        "longMin": {"type": "number"},
+        "longMax": {"type": "number"},
+        "startTime": {"type": "string"},
+        "endTime": {"type": "string"},
+        "cellWidth": {"type": "number"},
+        "cellHeight": {"type": "number"}
+    }
+}
+
+dataloader_schema = {
+    "type": "object",
+    "required": ["loader", "params"],
+    "additionalProperties": False,
+    "properties":{
+        "loader": {"type": "string"},
+        "params": {"type": "object"}
+        
+    }
+}
+
+splitting_schema = {
+    "type": "object",
+    "required": ["split_depth", "minimum_datapoints"],
+    "additionalProperties": False,
+    "properties":{
+        "split_depth": {"type": "integer"},
+        "minimum_datapoints": {"type": "integer"}
+    } 
+}
+
+mesh_schema = {
+    "type": "object",
+    "required":["config"],
+    "additionalProperties": False,
+    "properties": {
+        "config": {
+            "type": "object",
+            "required": ["Mesh_info"],
+            "additionalProperties": False,
+            "properties":{
+                "Mesh_info": {
+                    "type": "object",
+                    "required": ["Region", "Data_sources", "splitting"],
+                    "additionalProperties": False,
+                    "properties":{
+                        "Region": {
+                            "$ref": "#/region_schema"
+                        },
+                        "Data_sources": {
+                            "type": "array",
+                            "items": {
+                                "$ref": "#/dataloader_schema"
+                            },
+                        },
+                        "splitting": {
+                            "$ref": "#/splitting_schema"
+                        }
+                    }
+                }
+            }
+        }
+    },
+    "region_schema": region_schema,
+    "dataloader_schema": dataloader_schema,
+    "splitting_schema": splitting_schema
+}
+
+if __name__ == "__main__":
+    import jsonschema
+    import json
+    
+    with open("/home/habbot/Documents/Work/PolarRoute/examples/environment_config/bad.config.json", "r") as fp:    
+        config = json.load(fp)
+    try:    
+        jsonschema.validate(instance=config, schema=mesh_schema)
+        print("correct")
+    except jsonschema.exceptions.ValidationError as e:
+        print(e)
+    
