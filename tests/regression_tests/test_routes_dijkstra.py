3509b14a9f62a4400ea5da639a05a5035fa670e0
George <gecoomb@bas.ac.uk>
2024-04-09 15:30:09 +0100

    Add vertical crossing point tests

--- tests/regression_tests/test_routes_dijkstra.py
+++ tests/regression_tests/test_routes_dijkstra.py
@@ -40,7 +40,15 @@ TEST_ROUTES = [
     './example_routes/dijkstra/crossing_point/horizontal/horizontal_60lat_n.json',
     './example_routes/dijkstra/crossing_point/horizontal/horizontal_80lat_s.json',
     './example_routes/dijkstra/crossing_point/horizontal/horizontal_80lat_n.json',
-    './example_routes/dijkstra/crossing_point/diagonal/diagonal_0lat.json'
+    './example_routes/dijkstra/crossing_point/diagonal/diagonal_0lat.json',
+    './example_routes/dijkstra/crossing_point/vertical/vertical_20lat_s.json',
+    './example_routes/dijkstra/crossing_point/vertical/vertical_20lat_n.json',
+    './example_routes/dijkstra/crossing_point/vertical/vertical_40lat_s.json',
+    './example_routes/dijkstra/crossing_point/vertical/vertical_40lat_n.json',
+    './example_routes/dijkstra/crossing_point/vertical/vertical_60lat_s.json',
+    './example_routes/dijkstra/crossing_point/vertical/vertical_60lat_n.json',
+    './example_routes/dijkstra/crossing_point/vertical/vertical_80lat_s.json',
+    './example_routes/dijkstra/crossing_point/vertical/vertical_80lat_n.json'
 ]
 
 def setup_module():
