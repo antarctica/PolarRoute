52ae284960d710b05636d8667d3b0df8f2ecde57
George <gecoomb@bas.ac.uk>
2024-04-11 09:59:31 +0100

    Add vertical split case

--- tests/regression_tests/test_routes_dijkstra.py
+++ tests/regression_tests/test_routes_dijkstra.py
@@ -49,7 +49,8 @@ TEST_ROUTES = [
     './example_routes/dijkstra/crossing_point/vertical/vertical_60lat_n.json',
     './example_routes/dijkstra/crossing_point/vertical/vertical_80lat_s.json',
     './example_routes/dijkstra/crossing_point/vertical/vertical_80lat_n.json',
-    './example_routes/dijkstra/crossing_point/horizontal/horizontal_0lat_split.json'
+    './example_routes/dijkstra/crossing_point/horizontal/horizontal_0lat_split.json',
+    './example_routes/dijkstra/crossing_point/vertical/vertical_0lat_split.json'
 ]
 
 def setup_module():
